1175984faaa38b45e4351676e16b2e6398cd564e
Ekaterina Sergina <weenfry@mail.ru>
2022-08-24 02:35:41 +0300

    Added Exe 03

---
+++ Exe03/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число:");  
+int A = int.Parse(Console.ReadLine());  
+
+if (A % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else 
+{
+    Console.WriteLine("Нет");       
+}
+
