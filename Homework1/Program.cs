eb17c1df39bde695684e0fa4c390d7c90afc5cd7
Ekaterina Sergina <weenfry@mail.ru>
2022-08-24 02:04:27 +0300

    Added Homework 1

---
+++ Homework1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите первое число:"); 
+int a = int.Parse(Console.ReadLine()); 
+ 
+Console.WriteLine("Введите второе число:"); 
+int b = int.Parse(Console.ReadLine()); 
+ 
+int min = 0; 
+int max = 0; 
+ 
+if (a < b) { 
+    min = a; 
+    max = b; 
+} else if (b < a) { 
+    min = b; 
+    max = a; 
+} 
+ 
+if (min != max) { 
+    Console.WriteLine("Минимальное число " + min); 
+    Console.WriteLine("Максимальное число " + max); 
+} else { 
+    Console.WriteLine("Числа равны"); 
+}
