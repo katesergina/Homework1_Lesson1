1246c262cb3509812d9e8c85de21087e082aa1fb
Ekaterina Sergina <weenfry@mail.ru>
2022-08-24 02:38:07 +0300

    Added Exe 04

---
+++ Exe04/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число:");  
+int A = int.Parse(Console.ReadLine());  
+ 
+string s = ""; 
+for (int i = 1; i <= A; i++)
+{ 
+    if (i % 2 == 0)
+    { 
+        if (s.Length > 0)
+        { 
+            s += ", "; 
+        } 
+        s += i; 
+    } 
+} 
+Console.WriteLine(s);
