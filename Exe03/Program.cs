fa4601a495e882a2e9a11c3bfa4bf5b74a955fc7
Ekaterina Sergina <111311879+katesergina@users.noreply.github.com>
2022-08-29 19:53:05 +0300

    Update Program.cs

--- Exe03/Program.cs
+++ Exe03/Program.cs
@@ -1,12 +1,9 @@
-﻿Console.WriteLine("Введите число:");  
-int A = int.Parse(Console.ReadLine());  
-
-if (A % 2 == 0)
-{
-    Console.WriteLine("Да");
-}
-else 
-{
-    Console.WriteLine("Нет");       
-}
-
+Console.WriteLine("Enter a day number:"); 
+int n = int.Parse(Console.ReadLine()); 
+ 
+if (n < 1 || n > 7) { 
+    Console.WriteLine("Нет такого дня недели :-)"); 
+    Environment.Exit(1); 
+} 
+ 
+Console.WriteLine(n >= 6 ? "Да" : "Нет");
