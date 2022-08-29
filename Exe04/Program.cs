221105a7106ab17d262120fc5a853217bfd41ff1
Ekaterina Sergina <111311879+katesergina@users.noreply.github.com>
2022-08-29 19:53:37 +0300

    Update Program.cs

--- Exe04/Program.cs
+++ Exe04/Program.cs
@@ -1,16 +1,1 @@
-﻿Console.WriteLine("Введите число:");  
-int A = int.Parse(Console.ReadLine());  
- 
-string s = ""; 
-for (int i = 1; i <= A; i++)
-{ 
-    if (i % 2 == 0)
-    { 
-        if (s.Length > 0)
-        { 
-            s += ", "; 
-        } 
-        s += i; 
-    } 
-} 
-Console.WriteLine(s);
+
