4b8b1afa32947e9133dbfa35d6c0680a02173b5e
Ekaterina Sergina <111311879+katesergina@users.noreply.github.com>
2022-08-29 19:49:46 +0300

    Update Program.cs

--- Exe01/Program.cs
+++ Exe01/Program.cs
@@ -1,23 +1,3 @@
-﻿Console.WriteLine("Введите первое число:"); 
-int a = int.Parse(Console.ReadLine()); 
- 
-Console.WriteLine("Введите второе число:"); 
-int b = int.Parse(Console.ReadLine()); 
- 
-int min = 0; 
-int max = 0; 
- 
-if (a < b) { 
-    min = a; 
-    max = b; 
-} else if (b < a) { 
-    min = b; 
-    max = a; 
-} 
- 
-if (min != max) { 
-    Console.WriteLine("Минимальное число " + min); 
-    Console.WriteLine("Максимальное число " + max); 
-} else { 
-    Console.WriteLine("Числа равны"); 
-}
+Console.WriteLine("Enter the number:"); 
+string n = int.Parse(Console.ReadLine()).ToString(); 
+Console.WriteLine(n[1]);
